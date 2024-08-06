f1678274e5c785681c7354b2c6a577dfc4282f03
Janice Cheung <cheungayin@gmail.com>
2024-08-06 17:38:17 -0400

    big update
    reformatted some codes (in case i want to add filters in the future instead of copy paste), added more image for works, added touchdesigner, motion graphics

--- script.js
+++ script.js
@@ -13,3 +13,37 @@ toggleMenu.addEventListener( "click", () => {
 },
 false,
 );
+
+function checkFilters() {
+
+    let checked = [];
+
+    $('input[type="checkbox"] + label').css("opacity", 1);
+    $('input[type="checkbox"]:not(:checked) + label').css("opacity", 0.5);
+
+    if ($("#filter .filter-type input:checkbox:checked").length > 0) {
+
+        $('#filter .filter-type input[type="checkbox"]:checked').each(function () {
+        checked.push( "[" + $(this).attr("name") + "]");
+        });
+    } else{
+    $('input[type="checkbox"] + label').css("opacity", 1);
+    }
+
+    if (checked.length == 0 || checked == null) {
+        $("#i > *").css("opacity", 1).css("pointer-events", "auto");
+        return;
+    }
+
+    $(checked).each(function (i, e) {
+        $("#i > *" + e).css("opacity", 1).css("pointer-events", "auto");
+    });
+
+    };
+
+    // CHECKBOX 
+    $("#filter .filter-type input[type='checkbox']").change(function () {  
+    $("#i > *").css("opacity", 0).css("pointer-events", "none");
+    checkFilters();
+});
+  
